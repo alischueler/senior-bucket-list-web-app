10b3847425f8d519b2f745a1e29df804d9cd6463
Ali Schueler <ali.schueler456@gmail.com>
2023-05-02 12:38:35 -0400

    things

--- src/ShotTracker/ShotTrackerItem.js
+++ src/ShotTracker/ShotTrackerItem.js
@@ -2,16 +2,19 @@ import {useSelector} from "react-redux";
 
 const ShotTrackerItem = ({person}) => {
     const { completed } = useSelector(state => state.completedData)
+    const { drinks } = useSelector(state => state.drinksData)
     const num = completed.filter(c => {
-        console.log(c.person)
-        console.log(person.name)
+        return c.person === person.name}).length
+    const numDrinks = drinks.filter(c => {
         return c.person === person.name}).length
     return (
         <li className="list-group-item d-flex align-items-center">
             <img src={`/images/${person.profile}`} width="150px" alt="shot item"/>
             <div className="align-middle ps-3">
                 completed <b>{num}</b> bucket list items <br/>
-                has <b>{50-num}</b> shots to take
+                and needs to take <b>{50-num}</b> shots <br/>
+                she has taken <b>{numDrinks}</b> shots <br/>
+                and has <b>{50-num-numDrinks}</b> left to take
             </div>
         </li>
     )
